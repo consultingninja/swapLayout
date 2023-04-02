41dd80ec7398de5f06115bef71fa179b37651890
consultingninja <consultingninja.tech@gmail.com>
2023-04-02 18:52:59 -0500

    add message store

--- src/stores.js
+++ src/stores.js
@@ -1,9 +1,10 @@
 import { writable} from "svelte/store";
 
 
-export const layout = writable('top');
+
 
 export const showModal = writable(false);
-
+export const layout = writable('top');
 export const carousel = writable(false);
 export const hero = writable(false);
+export const message = writable('');
